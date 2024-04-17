1d03027f67b1fcb061f383d8e27ca2da377e78e4
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-17 10:02:24 -0600

    putting cars in appstate

--- src/services/CarsService.js
+++ src/services/CarsService.js
@@ -1,3 +1,5 @@
+import { AppState } from "../AppState.js";
+import { Car } from "../models/Car.js";
 import { logger } from "../utils/Logger.js";
 import { api } from "./AxiosService.js"
 
@@ -6,6 +8,7 @@ class CarsService {
     const response = await api.get('api/cars')
     // NOTE logger is similar to console, will not break your application during deployment
     logger.log('GOT CARS 🚗', response.data);
+    AppState.cars = response.data.map(pojo => new Car(pojo))
   }
 }
 
