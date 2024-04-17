287a11c4df269addaf9fc8d4ec440fe37742563a
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-17 09:42:54 -0600

    added routerlink to cars page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -31,8 +31,8 @@ function toggleTheme() {
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
         <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
+          <router-link :to="{ name: 'Cars' }" class="btn text-success lighten-30 selectable text-uppercase">
+            Cars
           </router-link>
         </li>
       </ul>
